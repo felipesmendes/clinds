924bedffe4a21dae5a0ed139926983953f21cd30
Felipe Mendes <contato@felipems.com.br>
2015-01-07 15:20:22 -0200

    Create reactive in google maps.

--- client/helpers/map.js
+++ client/helpers/map.js
@@ -1,6 +1,33 @@
 Template.map.rendered = function(){
-	Map.initialize("map_canvas");
 	if(Meteor.isCordova){
+			Map.initialize("map_canvas");
 		alert(Geolocation.currentLocation());
 	}
+	if(Meteor.isClient){
+	 	if (navigator.geolocation) {
+        	navigator.geolocation.watchPosition(function(position){
+        		var mapOptions = { 
+        			center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        			zoom: 16,
+            		mapTypeId: google.maps.MapTypeId.HYBRID
+            	};
+        		Map.initialize("map_canvas",mapOptions);
+        		Map.addMarker({lat:position.coords.latitude, lng:position.coords.longitude});
+				Tracker.autorun(function () {        		
+	        		var clubs = Club.find({loc: {$near:[position.coords.latitude,position.coords.longitude],$maxDistance: 0.08}}).fetch();
+	        		_.each(clubs,function(club){
+	        			Map.addMarker({lat:club.loc[0], lng:club.loc[1]});
+	        		});
+        		});
+        	});
+     	} else {
+	        alert("Geolocation is not supported by this browser.");
+	    }
+	}
 };
+
+Template.map.helpers({
+	clubs: function(){
+
+	}
+})
